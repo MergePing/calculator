6f50086051dec7e7e53852ea7eec0d1b5d5776c2
youngseok-1 <whdudtjr0@gmail.com>
2024-10-07 16:54:49 +0900

    divide

--- src/main/java/com/ohgiraffers/common/divide.java
+++ src/main/java/com/ohgiraffers/common/divide.java
@@ -3,8 +3,10 @@ package com.ohgiraffers.common;
 public class divide {
 
     public int divide (int num1, int num2) {
-
-        return num1/num2;
+        if (num2 == 0) {
+            throw new IllegalArgumentException("0으로 나눌수 없습니다.");
+        }
+        return num1 / num2;
 
     }
 
